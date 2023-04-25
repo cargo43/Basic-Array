2aa2919cb85e1e7269557044f157783e9823e801
cargo43 <98904950+cargo43@users.noreply.github.com>
2023-04-25 20:31:26 +0530

    Create Arrayinputoutput.c

---
+++ Arrayinputoutput.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int i;
+	int values[5];
+	
+	printf("Enter 5 integers: ");
+	
+	for 
+	{
+		scanf("%d",&values[i]);
+	}
+	printf("Displaying integers: ");
+	
+	for(int i=0;i<5;++i)
+	{
+		printf("%d \n",values[i]);
+	}
+	return 0;
+}
